4f855e43f35dcb55f4679daffb337ff165da03e5
visurel <hello@visurel.com>
2024-01-20 10:21:24 +0100

    add reproduction for mikro-orm issue

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Post('test')
+  getHello(@Body() body: any): string {
+    console.log('BODY', body);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// START AFFECTING CODE
+// When removing this part, the issue is gone
+import { Entity, PrimaryKey } from '@mikro-orm/core';
+
+@Entity()
+export class Test {
+  @PrimaryKey()
+  id!: number;
+}
+// END AFFECTING CODE
+
 @Module({
   imports: [],
   controllers: [AppController],
